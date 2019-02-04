0afcfbd9f751b364904377391531ebb9528116ea
Master_Cloud <lsclike2016@hotmail.com>
2019-02-04 11:56:00 +1100

    update linked list

--- Book/ch2/list.py
+++ Book/ch2/list.py
@@ -22,12 +22,34 @@ def len_link(s):
         s, length = rest(s), length + 1
     return length
 
+# recursive length
+def len_link_recursive(s):
+    if s == empty:
+        return 0
+    else:
+        return len_link_recursive(rest(s)) + 1
+
 def getitem_link(s, i):
     while i > 0:
         s = rest(s)
         i -= 1
     return first(s)
 
+def getitem_link_recursive(s, i):
+    if i == 0:
+        return first(s)
+    else:
+        return getitem_link_recursive(rest(s), i - 1)
+
+def extend_link(s, t):
+    assert is_list(s) and is_list(t)
+    if s == empty:
+        return t
+    else:
+        return link(first(s), extend_link(rest(s), t))
+
 if __name__ == '__main__':
     test_link = link(5,link(4, link(3, link(2, link(1,empty)))))
-    print(getitem_link(test_link, 0))
+    l1 = link(6, link(5, link(4, link(3, link(2, link(1, 'empty'))))))
+    result_link = extend_link(test_link, l1)
+    print(result_link)
