a1db7e65201385a1c5299381be2d4af7c1cea19e
Master_Cloud <lsclike2016@hotmail.com>
2021-04-03 22:58:09 +1100

    tree practice

--- Book/ch2/tree.py
+++ Book/ch2/tree.py
@@ -1,9 +1,3 @@
-def tree(root_label, branches=[]):
-    for branch in branches:
-        assert is_tree()
-
-    return [root_label] + list(branches)
-
 def label(tree):
     return tree[0]
 
@@ -13,3 +7,20 @@ def branches(tree):
 def is_tree(tree):
     if type(tree) != list or len(tree) < 1:
         return False
+    for branch in branches(tree):
+        if not is_tree(branch):
+            return False
+    return True
+
+def is_leaf(tree):
+    return not branches(tree)
+
+def tree(root_label, branches=[]):
+    for branch in branches:
+        assert is_tree(branch)
+
+    return [root_label] + list(branches)
+
+print(tree(1))
+
+print(tree(2))
