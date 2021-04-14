bcfe3a6dbefaca87ce24c08bd9a1a86e2a410bee
Master_Cloud <lsclike2016@hotmail.com>
2021-04-14 23:59:49 +1000

    test v4

--- Book/ch2/tree.py
+++ Book/ch2/tree.py
@@ -50,3 +50,11 @@ def partition_tree(n, m):
             left = partition_tree(n-m, m)
             right = partition_tree(n, m-1)
             return tree(m, [left, right])
+
+def right_binarize(tree):
+
+    if is_leaf(tree):
+        return tree
+    if len(tree) > 2:
+        tree = [tree[0], tree[1:]]
+    return [right_binarize(b) for b in tree]
