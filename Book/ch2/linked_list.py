8e7904c4a351ec35694ec3c60b2df05a4b8e4e27
Master_Cloud <lsclike2016@hotmail.com>
2021-04-19 23:58:20 +1000

    bring code for linked lists

--- Book/ch2/linked_list.py
+++ Book/ch2/linked_list.py
@@ -1,1 +1,14 @@
-"check linked list"
+def is_link(s):
+        return s == empty or (len(s) == 2 and is_link(s[1]))
+>>> def link(first, rest):
+        assert is_link(rest), "rest must be a linked list."
+        return [first, rest]
+>>> def first(s):
+
+        assert is_link(s), "first only applies to linked lists."
+        assert s != empty, "empty linked list has no first element."
+        return s[0]
+>>> def rest(s):
+        assert is_link(s), "rest only applies to linked lists."
+        assert s != empty, "empty linked list has no rest."
+        return s[1]
