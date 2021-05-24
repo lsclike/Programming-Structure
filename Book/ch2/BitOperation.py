2673eaf67ea03f0042de1beb9473f196f77448df
Master_Cloud <lsclike2016@hotmail.com>
2021-05-24 23:34:27 +1000

    bit operation

--- Book/ch2/BitOperation.py
+++ Book/ch2/BitOperation.py
@@ -1,1 +1,15 @@
 # start playing bit operations
+
+def count_even_odd(n):
+    even_count, odd_count = 0, 0
+    for i in range(0,n):
+        if i & 1 == 0:
+            even_count +=1
+        else:
+            odd_count +=1
+    
+    print(even_count)
+    print(odd_count)
+
+
+count_even_odd(100)
