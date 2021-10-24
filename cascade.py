b3afeb25dffeff6d242950490ead5e827ecd016e
Master_Cloud <lsclike2016@hotmail.com>
2021-10-24 23:49:43 +1100

    Add calculation logic for finbo

--- cascade.py
+++ cascade.py
@@ -6,3 +6,15 @@ def cascade_tree(n: int):
         cascade_tree(n//10)
         print(n)
 
+
+# a range value maps to an exit way
+def finbo(number):
+    if number <= 2:
+        return 1
+    else:
+        return finbo(number-1) + finbo(number-2)
+
+4
+
+f(3) + f(2)
+f(1) + f(2) + f(2)
