899c03a9660b49e01d62b51a7722d524c24d7b33
lsclike <22793889+lsclike@users.noreply.github.com>
2023-06-02 12:28:59 +1000

    local reference error

--- diagram.py
+++ diagram.py
@@ -1,9 +1,14 @@
 from random import randint
 
+def parent_is_parent_is_global():
+    return 1
+
+
 def parent_is_global():
+    parent_is_parent_is_global()
     def parent_is_parent_is_global():
         return 0
-    return 0
 
+    return 0
 
 parent_is_global()
