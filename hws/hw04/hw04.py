449c858066cfb8741e64ee70845f8cbef1903979
lsclike <22793889+lsclike@users.noreply.github.com>
2023-06-04 23:11:16 +1000

    Done total weight

--- hws/hw04/hw04.py
+++ hws/hw04/hw04.py
@@ -43,13 +43,13 @@ def end(s):
 def planet(size):
     """Construct a planet of some size."""
     assert size > 0
-    "*** YOUR CODE HERE ***"
+    return ['planet', int(size)]
 
 def size(w):
     """Select the size of a planet."""
     assert is_planet(w), 'must call size on a planet'
     "*** YOUR CODE HERE ***"
-
+    return w[1]
 def is_planet(w):
     """Whether w is a planet."""
     return type(w) == list and len(w) == 2 and w[0] == 'planet'
