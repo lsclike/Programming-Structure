9b87a39186a8871f3106791d795d079c739e6018
lsclike <22793889+lsclike@users.noreply.github.com>
2023-06-18 23:43:13 +1000

    Add first and rest functions

--- Lab/linked_list.py
+++ Lab/linked_list.py
@@ -7,3 +7,16 @@ def link(first, rest):
         """Construct a linked list from its first element and the rest."""
         assert is_link(rest), "rest must be a linked list."
         return [first, rest]
+
+
+def first(s):
+        """Return the first element of a linked list s."""
+        assert is_link(s), "first only applies to linked lists."
+        assert s != empty, "empty linked list has no first element."
+        return s[0]
+
+def rest(s):
+        """Return the rest of the elements of a linked list s."""
+        assert is_link(s), "rest only applies to linked lists."
+        assert s != empty, "empty linked list has no rest."
+        return s[1]
